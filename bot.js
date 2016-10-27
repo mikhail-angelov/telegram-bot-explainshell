a0c3b89bb1b1b8d0df28db8993023e3deea2d25b
Mikhail Angelov <mikhail.angelov@gmail.com>
2016-10-27 21:56:28 +0300

    refactor bot

--- bot.js
+++ bot.js
@@ -1,37 +1,24 @@
-const Bot = require('node-telegram-bot-api');
 const request = require('request');
 const cheerio = require('cheerio');
 const MAIN_URL = 'http://explainshell.com/explain';
-function init(token){
-	var bot;
-
-	if(process.env.NODE_ENV === 'production') {
-	  bot = new Bot(token);
-	  bot.setWebHook(process.env.HEROKU_URL + bot.token);
-	}
-	else {
-	  bot = new Bot(token, { polling: true });
-	}
-
-	bot.on('message',(msg)=>{
-		const qs = {cmd: msg.text}
-		request({url:MAIN_URL, qs:qs},(error, response, body)=>{
-			if(!error && response.statusCode == 200 && body){
-				const $ = cheerio.load(body);
-				//todo: format output
-				const data = $('#help').text() || 'parse html error :(';
-				bot.sendMessage(msg.chat.id, data, {parse_mode:'HTML'})
-		  			.then(function () {
-				    	// console.log('op',msg, data)
-					});
-			}else{
-				const message = 'explainshell.com site error: ' + error;
-				bot.sendMessage(msg.chat.id, message)				
-			}
-  		});
-		console.log('op',msg)
-	})
-	return bot;
+function processMessage(telegram,msg){
+	const qs = {cmd: msg.text}
+	request({url:MAIN_URL, qs:qs},(error, response, body)=>{
+		if(!error && response.statusCode == 200 && body){
+			const $ = cheerio.load(body);
+			//todo: format output
+			const data = $('#help').text() || 'parse html error :(';
+			telegram.sendMessage(msg.chat.id, data, {parse_mode:'HTML'})
+				.then(function () {
+				// console.log('op',msg, data)
+				});
+		}else{
+			const message = 'explainshell.com site error: ' + error;
+			telegram.sendMessage(msg.chat.id, message)				
+		}
+		});
+	console.log('op',msg)
 }
 
-module.exports = init;
+
+module.exports = {processMessage};
